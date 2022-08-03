24fd6874c63c42aa57d3fe2b567c83cf755c78ed
João Gallo <joaogallop@gmail.com>
2022-08-03 20:55:23 -0300

    Adiciona requisito 2 - agora ta tranquilo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
-    <Route path="/" component={ Login } />
+    <div>
+      <Route exact path="/" component={ Login } />
+      <Route path="/carteira" component={ Wallet } />
+    </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Header extends Component {
+class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <div>
+        <fieldset>
+          <p data-testid="email-field">{ email }</p>
+          {' '}
+          <spam data-testid="total-field">0 </spam>
+          <spam data-testid="header-currency-field">BRL</spam>
+        </fieldset>
+      </div>
     );
   }
 }
+
+Header.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
